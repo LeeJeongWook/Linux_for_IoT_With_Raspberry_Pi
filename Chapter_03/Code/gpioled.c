b583a0a7836b10a67ae0de3ea88b4dc22b2ef01c
LeeJeongWook <leejw0709@gmail.com>
2022-06-04 20:09:08 +0900

    --allow-empty-message

--- Chapter_03/Code/gpioled.c
+++ Chapter_03/Code/gpioled.c
@@ -6,7 +6,43 @@
 
 int ledControl(int gpio)
 {
-	int df;
-	char buf[BUFSIZ];
+	int fd;
+	fd = open("/sys/class/gpio/export", O_WRONLY);				/* 해당 GPIO 디바이스 사용 준비 */
+	sprintf(buf, "%d", gpio);
+	write(fd, buf, strlen(buf));
+	close(fd);
 
-	fd = open
+	sprintf(buf, "/sys/class/gpio/gpio%d/direction", gpio);		/* 해당 GPIO 디바이스의 방향 설정 */
+	fd = open(buf, O_WRONLY);
+	/* LED를 위해 디바이스를 출력으로 설정 : 입력의 경우 write(df, "in", 3); 사용 */
+	write(fd, "out", 4);
+	close(fd);
+
+	sprintf(buf, "/sys/class/gpio/gpio%d/value", gpio);			/* 디바이스에 값 출력 */
+	fd = open(buf, O_WRONLY);
+	wrtie(fd, "1", 2);
+	close(fd);
+
+	getchar();					/* LED확인을 위한 대기 */
+
+	fd = open("/sys/class/gpio/unexport", O_WRONLY);			/* 사용한 GPIO 디바이스 해제하기 */
+	sprintf(buf, %d, gpio);
+	write(fd, buf, strlen(buf));
+	close(fd);
+
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	int gno;
+
+	if(argc < 2){
+		printf("Usage : %s GPIO_NO\n", argv[0]);
+		return -1;
+	}
+	gno = atoi(argv[1]);
+	ledControl(gno);
+
+	return 0;
+}
